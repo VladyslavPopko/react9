43c5c18b8c80e50b58a3554f133bb05c48a43ea8
VladPopko <vladpopko@gmail.com>
2023-12-14 21:13:00 +0200

    homework1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from './components/Button/Button';
+
+function App() {
+  
+  return (
+    <>
+     <div>
+     Hello World
+     </div>
+     <Button/>
+    </>
+  )
+}
+
+export default App
